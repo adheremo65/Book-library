48a2d9adbc82fc4bbd83031ff44d1749ce3d2d7f
Abdela Umer <adheremo65@gmail.com>
2023-09-30 10:50:01 -0400

    first commit

--- python-p5-book-library-project/server/app.py
+++ python-p5-book-library-project/server/app.py
@@ -5,6 +5,7 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
+#try
 
 # Local imports
 from config import app, db, api
@@ -16,7 +17,8 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
-
+def apply():
+    print("hello")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
